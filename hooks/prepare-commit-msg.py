189b5f1189a073e727f15db5a34ebcfb64e21148
Maximilian S <maximilian.schreiter@t-online.de>
2020-06-22 10:38:21 +0200

    SHOULD BE THIS NOW

--- hooks/prepare-commit-msg.py
+++ hooks/prepare-commit-msg.py
@@ -1,20 +1,33 @@
 import sys
 
+
 class CustomStdOut:
     def __init__(self):
-        self.old_stdout=sys.stdout
+        self.old_stdout = sys.stdout
 
     def write(self, string):
         string = string.rstrip()
-        if len(string) == 0 : return
+        if len(string) == 0: return
         self.old_stdout.write(string)
 
 
 def main():
     out = CustomStdOut()
-    out.write(str(sys.argv) + "\n")
-    out.write("SUCCESS\n")
+
+    commit_msg_path = sys.argv.__getitem__(4)
+    out.write(commit_msg_path + "\n")
+    commit_msg_file = open(commit_msg_path, "a+")
+    commit_msg_file.seek(0)
+    string = commit_msg_file.read()
+    out.write(string)
+    commit_msg_file.seek(0)
+    commit_msg_file.write("SHOULD BE THIS NOW")
+
+    # out.write(str(sys.argv) + "\n")
+    # out.write("SUCCESS\n")
+    commit_msg_file.close()
     sys.exit(1)
+
 
 if __name__ == '__main__':
     main()
