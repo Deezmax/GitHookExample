65bf8d31d1c27cc971bee2df3049e5cf43bbf9ec
Maximilian Schreiter <maximilian.schreiter@t-online.de>
2020-07-30 12:56:57 +0200

    TEst

--- hooks/prepare_commit_msg.py
+++ hooks/prepare_commit_msg.py
@@ -1,17 +1,23 @@
+"""Writes something into the commit msg"""
 import sys
 
 
 class CustomStdOut:
+    """Output class"""
     def __init__(self):
+        """Constructor"""
         self.old_stdout = sys.stdout
 
     def write(self, string):
+        """Write a given string to console"""
         string = string.rstrip()
-        if len(string) == 0: return
+        if len(string) == 0:
+            return
         self.old_stdout.write(string)
 
 
 def main():
+    """MAIN"""
     out = CustomStdOut()
 
     commit_msg_path = sys.argv.__getitem__(4)
@@ -21,6 +27,7 @@ def main():
     string = commit_msg_file.read()
     out.write(string)
     commit_msg_file.seek(0)
+    commit_msg_file.write("SHOULD BE THIS NOW")
     commit_msg_file.write("SHOULD BE THIS NOW")
 
     # out.write(str(sys.argv) + "\n")
