ab6a0a186c32e4f16038680effd56496deb06a68
Maximilian Schreiter <maximilian.schreiter@t-online.de>
2020-08-10 12:58:59 +0200

    feat: add check_file
    check_file scans every commited py file for a well formated name

---
+++ src/check_file.py
@@ -0,0 +1,49 @@
+"""A module to check a given word if it is in a certain format"""
+import re
+import sys
+
+
+def check_filename(word):
+    """Checks a given word w if its in filename format"""
+    result_regex = re.compile('[a-z]+(_[a-z]+)*.py$')
+    res = result_regex.match(word)
+    return res
+
+
+def main():
+    """Main function that executes on start"""
+    files = sys.argv[1].split(" ")
+
+    sys.stdout.write("Checking these files for naming:")
+    sys.stdout.write(str(files))
+
+    errors = []
+
+    for file in files:
+
+        if not file.endswith(".py"):
+            sys.stdout.write("\n Skipped file: %s" % file)
+            continue
+
+        split = file.rsplit("/")
+        if len(split) < 0:
+            sys.stderr.write("\nBIG FAT ERROR")
+            sys.exit(1)
+
+        word = split[len(split) - 1]
+
+        result = check_filename(word)
+
+        if result is None:
+            sys.stdout.write("\n" + file + " is in wrong format!")
+            errors.append(file)
+
+    if len(errors) > 0:
+        sys.stdout.write("\nRename your files and try again.")
+        sys.exit(1)
+
+    sys.exit(0)
+
+
+if __name__ == '__main__':
+    main()
