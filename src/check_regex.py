bc0d5aee02d6faecbc554273f770f49f3ab3c6a0
Maximilian Schreiter <maximilian.schreiter@t-online.de>
2020-08-10 10:45:10 +0200

    feat: check message with regex

--- src/check_regex.py
+++ src/check_regex.py
@@ -1,20 +1,40 @@
+"""A package to check a given word if it is in a certain format"""
 import re
-
-regex_branch = "^(feature|hotfixes|test|bug|refactor)\/([a-z]|[A-Z]|[0-9])+$"
-regex_commit = "^(docs|feat|fix|refactor|style|test)\:\s([a-z]|[A-Z]|\s|[0-9])+\.?$"
+import sys
 
 
-def check_for_regex(w, regex):
-    """Checks a given word w if its regex"""
-    result_regex = re.compile(regex)
-    res = result_regex.match(w)
+def check_commit(word):
+    """Checks a given word w if its in commit format"""
+    result_regex = re.compile('(docs|feat|fix|refactor|style|test):\\s([a-z]|[A-Z]|\\s|[0-9]|\\.)+?')
+    res = result_regex.match(word)
+    return res
+
+
+def check_branch(word):
+    """Checks a given word w if its in branch format"""
+    result_regex = re.compile('(feature|hotfixes|test|bug|refactor)/([a-z]|[A-Z]|[0-9])+')
+    res = result_regex.match(word)
     return res
 
 
 def main():
     """MAIN METHOD"""
-    #TODO check for branch commit
-    print(check_for_regex("feature/abc", regex_branch))
+
+    commit_msg_path = sys.argv.__getitem__(1)
+    commit_msg_file = open(commit_msg_path, "w+")
+    commit_msg_file.seek(0)
+    message = commit_msg_file.read()
+    sys.stdout.write(message)
+    commit_msg_file.seek(0)
+    commit_msg_file.close()
+
+    result = check_commit(message)
+
+    if result is None:
+        sys.exit(1)
+
+    sys.stdout.write("Your commit-message was accepted")
+    sys.exit(0)
 
 
 if __name__ == '__main__':
